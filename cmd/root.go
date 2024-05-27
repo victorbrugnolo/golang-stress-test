12336d513df1112d34a7cb0c00d3a7289ca1f82b
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-27 19:33:19 -0300

    get parameters from cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,32 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "golang-stress-test",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A tool to make stress tests using golang",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url, _ := cmd.Flags().GetString("url")
+		requests, _ := cmd.Flags().GetInt("requests")
+		concurrency, _ := cmd.Flags().GetInt("concurrency")
+
+		fmt.Printf("URL: %s\n", url)
+		fmt.Printf("Requests: %d\n", requests)
+		fmt.Printf("Concurrency: %d\n", concurrency)
+
+		return nil
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,15 +35,10 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.golang-stress-test.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("url", "u", "", "URL where the stress tests will be performed")
+	rootCmd.Flags().IntP("requests", "r", 0, "Number of requests")
+	rootCmd.Flags().IntP("concurrency", "c", 0, "Number of concurrent requests")
+	rootCmd.MarkFlagRequired("url")
+	rootCmd.MarkFlagRequired("requests")
+	rootCmd.MarkFlagRequired("concurrency")
 }
-
-
